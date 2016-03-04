4eb6392dc9b22009db2e4efb948fd6f9593537b6
bugrauluyurt <bugra.u@gmail.com>
2016-03-04 07:07:00 +0200

    Removed insecure package, created methods and updated the events section accordingly

--- todos.js
+++ todos.js
@@ -8,14 +8,12 @@ if (Meteor.isClient) {
     }
   });
 
+  // Events
   Template.main.events({
     "submit .new-todo": function(event) {
       var text = event.target.text.value;
       
-      Todos.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call('addTodo', text);
 
       // Clear Form
       event.target.text.value = '';
@@ -25,17 +23,44 @@ if (Meteor.isClient) {
     },
 
     "click .toggle-checked": function(){
-      Todos.update(this._id, {$set:{checked: ! this.checked}});
+      Meteor.call('setChecked', this._id, !this.checked);
     },
 
     "click .delete-todo": function(){
       if (confirm('Are You Sure')) {
-        Todos.remove(this._id);
+        Meteor.call('deleteTodo', this._id);
       }; 
     }
+  });
 
+  Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY' //  One of 'USERNAME_AND_EMAIL', 'USERNAME_AND_OPTIONAL_EMAIL', 'USERNAME_ONLY', or 'EMAIL_ONLY' (default).
   });
 }
+
+// Methods should be created after removing insecure package.
+// Methods are similar to above written events.
+// After completing the methods section you should change the events by below mentioned methods.
+// Below mentioned methods should be called above in the events section by Meteor.call()
+Meteor.methods({
+  addTodo: function(text){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+    Todos.insert({
+        text: text,
+        createdAt: new Date(),
+        userID: Meteor.userId(),
+        username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId){
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoID, setChecked){
+    Todos.update(todoID, {$set:{checked: setChecked}});
+  } 
+});
 
 if (Meteor.isServer) {
   
